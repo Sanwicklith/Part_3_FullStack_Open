fda87d5e81de64341328dd7804b442202ba19e70
Your Name <Sanwicklith>
2024-08-17 11:50:49 +0200

    fix trailing spaces with pax eslint --fix

--- models/person.js
+++ models/person.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.set('strictQuery', false)
-
-
-const url = process.env.MONGODB_URI
+mongoose.set('strictQuery', false);
 
 
-console.log('connecting to', url)
+const url = process.env.MONGODB_URI;
+
+
+console.log('connecting to', url);
 
 mongoose.connect(url)
 
   .then(result => {
-    console.log('connected to MongoDB')
+    console.log('connected to MongoDB');
   })
   .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -34,15 +34,15 @@ const personSchema = new mongoose.Schema({
     },
     required: true,
   }
-})
+});
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
